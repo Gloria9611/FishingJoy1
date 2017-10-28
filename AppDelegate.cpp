2b570e84444532af7cff627bb4a104bd21947f36
Gloria9611 <32032246+Gloria9611@users.noreply.github.com>
2017-10-29 00:22:31 +0800

    Add files via upload

---
+++ AppDelegate.cpp
@@ -0,0 +1,45 @@
+#include "AppDelegate.h"
+#include "GameScene.h"
+#include "GameMenuLayer.h"
+
+USING_NS_CC;
+
+AppDelegate::AppDelegate() {
+
+}
+
+AppDelegate::~AppDelegate() 
+{
+}
+
+bool AppDelegate::applicationDidFinishLaunching() {
+ 
+    CCDirector* pDirector = CCDirector::sharedDirector();
+    CCEGLView* pEGLView = CCEGLView::sharedOpenGLView();
+
+    pDirector->setOpenGLView(pEGLView);
+	
+  
+    pDirector->setDisplayStats(true);
+
+    
+    pDirector->setAnimationInterval(1.0 / 60);
+
+    
+    CCScene *pScene = GameMenuLayer::scene();
+
+
+    pDirector->runWithScene(pScene);
+
+    return true;
+}
+
+void AppDelegate::applicationDidEnterBackground() {
+    CCDirector::sharedDirector()->stopAnimation();
+
+}
+
+void AppDelegate::applicationWillEnterForeground() {
+    CCDirector::sharedDirector()->startAnimation();
+
+}
---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#define WIN32_LEAN_AND_MEAN            
+// Windows Header Files:
+#include <windows.h>
+#include <tchar.h>
+
+// C RunTime Header Files
+//#include "CCStdC.h"
+
+#endif    // __MAIN_H__
