480caaa5bcb11a1660bdc6f2c7f02fd50b099b17
Gloria9611 <32032246+Gloria9611@users.noreply.github.com>
2017-12-29 16:35:42 +0800

    Add files via upload

--- Counter.cpp
+++ Counter.cpp
@@ -1,93 +1,80 @@
 #include "Counter.h"
 
-
 enum{
-	k_Counter_Action=0
+    k_Counter_Action = 0
 };
 
-Counter *Counter::create(CCArray* presenters,int digit)
+Counter* Counter::create(CCArray* presenters, int digit/* = 0 */)
 {
-	CCAssert(digit>=0&&digit<10,"金币数字只能为0至9之间的数字");
-	Counter *counter=new Counter;
-	if(counter && counter->init(presenters,digit))
-	{
+	CCAssert(digit>=0 && digit<=10, "");
+    Counter *counter = new Counter;
+	if (counter && counter->init(presenters,digit) )
+	{   
 		counter->autorelease();
 		return counter;
 	}
 	else
 	{
-
 		CC_SAFE_DELETE(counter);
 		return NULL;
 	}
+
 }
 
-bool Counter::init(CCArray* presenters,int digit)
+bool Counter::init(CCArray* presenters, int digit/* = 0 */)
 {
-	if(!CCNode::init())
+	if (!CCNode::init())
 	{
 		return false;
 	}
+    _presenters = CCNode::create();
 
-	_presenters=CCNode::create();
-	CCObject *obj=NULL;
-	int i=0;
-	
-	/*CCARRAY_FOREACH(presenters,obj)
+	CCObject *obj = NULL;
+	int i = 0;
+	CCARRAY_FOREACH(presenters,obj)
 	{
-		CCNode *node=(CCNode *) obj;
+		CCNode *node = (CCNode *)obj;
 		_presenters->addChild(node,0,i);
-		node->setPosition(ccp(0,node->getContentSize().height*i));
+		node->setPosition(ccp(0,node->getContentSize().height * i));
 		i++;
 	}
-	this->addChild(_presenters);
-	setDigit(digit);*/
-
-	for(int i=0;i<10;i++)
-	{
-		
-		CCNode *node=(CCNode *)presenters->objectAtIndex(i);
-		float height=node->getContentSize().height;
-		_presenters->addChild(node,0,i);
-		node->setPosition(ccp(0,height*i));
-	}
-	addChild(_presenters);
-	setDigit(digit);
-	return true;
+    this->addChild(_presenters);
+    setDigit(digit);
+    return true;
 }
 
+void Counter::setDigit(int digit)
+{
+    if(_digit != digit)
+	{
+        _digit = digit;
+        animation();
+    }
+}
 
 int Counter::getDigit()
 {
-	return _digit;
+    return _digit;
 }
-
-void Counter::setDigit(int digit){
-
-if(_digit!=digit){
-	_digit=digit;
-	animation();
-}
-}
-
 
 void Counter::animation()
 {
-	_presenters->stopActionByTag(k_Counter_Action);
-	CCNode *node=(CCNode *)_presenters->getChildByTag(_digit);
-	CCPoint position=node->getPosition();
-	CCMoveTo *moveTo=CCMoveTo::create(0.3f,ccp(0,-position.y));
+	CCNode* pres = (CCNode*)_presenters->getChildByTag(_digit);
+    CCPoint dest = pres->getPosition();
+    _presenters->stopActionByTag(k_Counter_Action);
+    CCMoveTo* moveTo = CCMoveTo::create(0.3f, ccp(0, -dest.y));
 	moveTo->setTag(k_Counter_Action);
-	_presenters->runAction(moveTo);
+    _presenters->runAction(moveTo);
 }
+
 
 void Counter::visit()
 {
-	glEnable(GL_SCISSOR_TEST);
-	CCPoint position=getParent()->convertToWorldSpace(getPosition());
-	CCSize size =((CCNode *)_presenters->getChildByTag(_digit))->getContentSize();
-	glScissor((position.x-size.width/2)*0.4,(position.y-size.height/2)*0.4,size.width*0.4,size.height*0.4);
-	CCNode::visit();
-	glDisable(GL_SCISSOR_TEST);
+    glEnable(GL_SCISSOR_TEST);
+    CCSize size = ((CCNode *)_presenters->getChildByTag(_digit))->getContentSize();
+    CCPoint location = getParent()->convertToWorldSpace(getPosition());
+    glScissor((location.x - size.width / 2) * 0.4, (location.y - size.height / 2) * 0.4, size.width* 0.4, size.height* 0.4);
 
+    CCNode::visit();
+    glDisable(GL_SCISSOR_TEST);
 }
